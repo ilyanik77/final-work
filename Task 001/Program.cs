22d71d2e7bb33cf75d62eb2c3c0c0cb975c6ee87
ilyanik77 <ilyakhlamov77@gmail.com>
2022-09-21 12:32:48 +0300

    add Task 001

---
+++ Task 001/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу,которая из имеющегося массива строк формирует массив строк,
+// длина которых меньше либо равна 3 символам.
+
+Console.Write("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayStrings = new string[size];
+
+int len = 3;
+for (int i = 0; i < size; i++)
+{
+ Console.WriteLine($"Введите {i+1}-й элемент: ");
+ string element = Console.ReadLine();
+ if (element.Length <= len)
+    {
+        arrayStrings[i] = element;
+
+    }
+}
+Console.WriteLine();
+PrintArray(arrayStrings);
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
